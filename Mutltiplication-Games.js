d7ad12962a93dc8f71d31ff0e951bc8b17cac0fa
Paras <pkaittan123@gmail.com>
2025-02-27 19:12:06 +0530

    initial commit

--- Mutltiplication-Games.js
+++ Mutltiplication-Games.js
@@ -10,18 +10,22 @@ submit.addEventListener("click", () => {
   val = Number(val);
   let mult =
     document.querySelector("#num1").textContent * 
-    document.querySelector("#num2").textContent;
-  mult = Number(mult);
-  if (document.querySelector("#input").value === "") {
-    alert("Please enter a number");
+    document.querySelector("#num2").textContent;  
+  mult = Number(mult); 
+  if (document.querySelector("#input").value === "") { 
+    alert("Please enter a number"); 
+
   } else if (val === mult) {
-    console.log("inside if");
+    console.log("inside if");    
     count++;
+
     score.textContent = count;
     num1.textContent = Math.floor(Math.random() * 20);
     num2.textContent = Math.floor(Math.random() * 20);
+
   } else {
     num1.textContent = Math.floor(Math.random() * 20);
     num2.textContent = Math.floor(Math.random() * 20);
   }
+  
 });
